9a94bf50aeaf79ec0b4e448d75c7bf11e4b6c196
agul1no <augusto.cera@gmail.com>
2022-04-24 11:48:21 +0200

    unnecessary code from Player class removed

--- app/src/main/java/com/example/gameloop/Player.kt
+++ app/src/main/java/com/example/gameloop/Player.kt
@@ -1,38 +1,22 @@
 package com.example.gameloop
 
 import android.content.Context
-import android.content.res.Resources
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.Rect
-import android.graphics.drawable.Drawable
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
 
 class Player(context: Context) {
 
-    private val image: Drawable? = ContextCompat.getDrawable(context, R.mipmap.jetpack_logo)
-    private var image2 = BitmapFactory.decodeResource(context.resources,R.mipmap.jetpack_logo_player)
-    private var positionX: Double = 0.0
-    private var positionY: Double = 0.0
+    private var imageAndroidPlayer = BitmapFactory.decodeResource(context.resources,R.mipmap.jetpack_logo_player)
     var paint: Paint = Paint()
 
     fun draw(canvas: Canvas, playerX: Int, playerY: Int){
-        image?.setBounds(2,2,2,2)
-        image?.draw(canvas)
-
-        //canvas.drawBitmap(image2, null , Rect(playerX,playerY,1000,1000),paint)
-        canvas.drawBitmap(image2, playerX.toFloat()-200,playerY.toFloat()-400,paint)
+        canvas.drawBitmap(imageAndroidPlayer, playerX.toFloat()-200,playerY.toFloat()-400,paint)
     }
 
     fun update(){
 
     }
 
-    fun setPosition(positionX: Double,positionY:Double){
-        this.positionX = positionX
-        this.positionY = positionY
-    }
+
 }
