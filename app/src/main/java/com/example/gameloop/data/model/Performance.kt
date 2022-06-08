6aabda86a4173d3b86caa3836d9c6231c13163bb
agul1no <augusto.cera@gmail.com>
2022-06-08 18:22:47 +0200

    Performance model class created

---
+++ app/src/main/java/com/example/gameloop/data/model/Performance.kt
@@ -0,0 +1,9 @@
+package com.example.gameloop.data.model
+
+data class Performance(
+    val playerID: Long,
+    val playerName: String,
+    val score: Int,
+    val timePlayed: String,
+    val timeStamp: Long
+)
